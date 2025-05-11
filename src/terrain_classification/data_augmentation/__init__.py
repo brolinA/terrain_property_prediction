b811ea4fba0544dd7724b1cedb5927a205211648
Brolin <abrolin56@gmail.com>
2025-05-11 14:27:07 +0200

    adding module definition for data_augmentation

---
+++ src/terrain_classification/data_augmentation/__init__.py
@@ -0,0 +1,6 @@
+#
+# Copyright (c) 2025-2027, RWTH Aaachen, Brolin Antony.
+# All rights reserved. Licensed under the MIT license.
+# See LICENSE file in the project root for details.
+#
+from .data_augmentation import DataAugmentation, test_data_augmentation
